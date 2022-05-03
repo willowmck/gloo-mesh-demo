0eeb6773e80cea97c581a9b209e2817b389028f7
Will McKinley <will.mckinley@solo.io>
2022-05-03 12:15:09 -0400

    Updated for lab12

--- 2.x/10-virtual-destination/08-scaleup.sh
+++ 2.x/10-virtual-destination/08-scaleup.sh
@@ -3,4 +3,7 @@
 CLUSTER1=cluster1
 
 kubectl --context ${CLUSTER1} -n bookinfo-frontends scale deploy/productpage-v1 --replicas=1
+
+sleep 20
+
 kubectl --context ${CLUSTER1} -n bookinfo-frontends wait --for=jsonpath='{.status.readyReplicas}'=1 deploy/productpage-v1
