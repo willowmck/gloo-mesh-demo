ab80c6f9d54bb336d21b47808f8b31ef952f8338
Will McKinley <will.mckinley@solo.io>
2022-05-04 10:54:23 -0400

    Updated for lab13

---
+++ 2.x/11-zero-trust/01-call-unsafe.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CLUSTER1=cluster1 
+
+#pod=$(kubectl --context ${CLUSTER1} -n httpbin get pods -l app=not-in-mesh -o jsonpath='{.items[0].metadata.name}')
+#kubectl --context ${CLUSTER1} -n httpbin debug -i -q ${pod} --image=curlimages/curl -- curl -s -o /dev/null -w "%{http_code}" \
+#    http://reviews.bookinfo-backends.svc.cluster.local:9080/reviews/0
+kubectl --context ${CLUSTER1} run -i -n httpbin --rm --restart=Never dummy --image=curlimages/curl --command -- \
+sh -c 'curl -s -w "%{http_code}" http://reviews.bookinfo-backends.svc.cluster.local:9080/reviews/0'
---
+++ 2.x/11-zero-trust/02-call-safe.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CLUSTER1=cluster1
+
+if [[ `kubectl get ns | grep foo` == 1 ]]; then
+    kubectl --context ${CLUSTER1} create ns foo
+    kubectl --context ${CLUSTER1} label ns foo istio-injection=enabled
+
+    echo "Deployng sleep into namespace foo"
+    kubectl --context ${CLUSTER1} apply -f sleep.yaml -n foo
+
+    sleep 20
+fi
+
+#kubectl --context ${CLUSTER1} -n httpbin debug -i -q ${pod} --image=curlimages/curl -- curl -s -o /dev/null -w "%{http_code}" \
+#    http://reviews.bookinfo-backends.svc.cluster.local:9080/reviews/0
+
+echo ""
+echo "Using sleep to curl reviews"
+echo ""
+kubectl --context ${CLUSTER1} exec "$(kubectl --context ${CLUSTER1} get pod -l app=sleep -n foo -o jsonpath={.items..metadata.name})" \
+    -c sleep -n foo -- curl -s -w "%{http_code}" http://reviews.bookinfo-backends.svc.cluster.local:9080/reviews/0
